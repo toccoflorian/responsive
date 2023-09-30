4f5a4dbef9ee3c989f5fb03deaef5e0bdbf3a268
Florian <t.florian181181@gmail.com>
2023-09-30 15:25:12 +0200

    30/09/23

--- formulaire.bundle.js
+++ formulaire.bundle.js
@@ -1420,7 +1420,7 @@ document.getElementById("formulaire-submit-button").addEventListener("click", ev
   const formData = new FormData(document.getElementById("formulaire"));
   const rep = fetch("http://164.132.229.216:6600/", {
     method: "POST",
-    mode: "same-origin",
+    // mode: "no-cors",
     body: formData
   });
   rep.then(response => response.json()).then(value => {
